b140a813e9be2f719b315bf01290bbc75311688b
Vaibhav Kumar Choudhary <vaibhavkumar81990@gmail.com>
2023-08-19 14:02:18 +0530

    vaibhav

--- main.js
+++ main.js
@@ -1,28 +1,44 @@
-const tabItems = document.querySelectorAll('.tab-item');
-const tabContentItems = document.querySelectorAll('.tab-content-item');
- 
+// -------- Login and Signup page --------------
+
+var LoginForm = document.getElementById("LoginForm");
+var RegForm = document.getElementById("RegForm");
+var Indicator = document.getElementById("Indicator");
+
+function register() {
+  RegForm.style.transform = "translateX(0px)";
+  LoginForm.style.transform = "translateX(0px)";
+  Indicator.style.transform = "translateX(100px)";
+}
+function login() {
+  RegForm.style.transform = "translateX(300px)";
+  LoginForm.style.transform = "translateX(300px)";
+  Indicator.style.transform = "translateX(0px)";
+}
+
+const tabItems = document.querySelectorAll(".tab-item");
+const tabContentItems = document.querySelectorAll(".tab-content-item");
+
 // Select tab Content Item
 
-function SelectItem(e){
-    removeBorder();
-    removeShow();
-    // Add Border to current tab
-    this.classList.add('tab-border');
+function SelectItem(e) {
+  removeBorder();
+  removeShow();
+  // Add Border to current tab
+  this.classList.add("tab-border");
 
-    // Grab content item from DOM
-    const tabContentItem =document.querySelector(`#${this.id}-content`);
-    // Add show class
-    tabContentItem.classList.add('show');
+  // Grab content item from DOM
+  const tabContentItem = document.querySelector(`#${this.id}-content`);
+  // Add show class
+  tabContentItem.classList.add("show");
 }
 
-function removeBorder(){
-    tabItems.forEach(item => item.classList.remove('tab-border'));
+function removeBorder() {
+  tabItems.forEach((item) => item.classList.remove("tab-border"));
 }
-function removeShow(){
-    tabContentItems.forEach(item => item.classList.remove('show'));
+function removeShow() {
+  tabContentItems.forEach((item) => item.classList.remove("show"));
 }
 
 // Listen for tab click
 
-tabItems.forEach(item => item.addEventListener('click', SelectItem));
-
+tabItems.forEach((item) => item.addEventListener("click", SelectItem));
